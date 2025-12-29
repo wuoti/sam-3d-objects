3113a82c0e9a12ca5401cedf616cb77f4ee7d69f
Kalle Wuoti <kalle.wuoti@gmail.com>
2025-12-29 14:12:36 +0100

    Add test image

--- demo.py
+++ demo.py
@@ -11,8 +11,8 @@ config_path = f"checkpoints/{tag}/pipeline.yaml"
 inference = Inference(config_path, compile=False)
 
 # load image (RGBA only, mask is embedded in the alpha channel)
-image = load_image("notebook/images/shutterstock_stylish_kidsroom_1640806567/image.png")
-mask = load_single_mask("notebook/images/shutterstock_stylish_kidsroom_1640806567", index=14)
+image = load_image("notebook/images/chair/chair.png")
+mask = None
 
 # Optional USD export path (set to a filename to enable)
 usd_path = "reconstruction.usd"  # e.g., "reconstruction.usd"
